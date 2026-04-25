b477171640f68b05210c7316eb55e056c567ef4d
intensecheese <146556801+sammack3252@users.noreply.github.com>
2026-04-24 22:14:02 -0500

    Update Form1.Designer.cs
    outfit implementation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Idol_Clicker
+namespace Idol_Clicker
 {
     partial class Form1
     {
@@ -38,6 +38,11 @@
             earningGemsToolStripMenuItem = new ToolStripMenuItem();
             unlockingOutfitsToolStripMenuItem = new ToolStripMenuItem();
             outfitsToolStripMenuItem = new ToolStripMenuItem();
+            rockBandToolStripMenuItem = new ToolStripMenuItem();
+            rockBandToolStripMenuItem1 = new ToolStripMenuItem();
+            stageToolStripMenuItem = new ToolStripMenuItem();
+            streetToolStripMenuItem = new ToolStripMenuItem();
+            catGirlToolStripMenuItem = new ToolStripMenuItem();
             exitToolStripMenuItem = new ToolStripMenuItem();
             textBox1 = new TextBox();
             label4 = new Label();
@@ -53,9 +58,23 @@
             label9 = new Label();
             button5 = new Button();
             button6 = new Button();
+            button7 = new Button();
+            pictureBox3 = new PictureBox();
+            label10 = new Label();
+            button8 = new Button();
+            pictureBox4 = new PictureBox();
+            button9 = new Button();
+            button10 = new Button();
+            pictureBox5 = new PictureBox();
+            button11 = new Button();
+            pictureBox6 = new PictureBox();
             menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -118,7 +137,7 @@
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Juice ITC", 20F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             button2.ForeColor = SystemColors.MenuHighlight;
-            button2.Location = new Point(291, 359);
+            button2.Location = new Point(291, 407);
             button2.Name = "button2";
             button2.Size = new Size(372, 49);
             button2.TabIndex = 4;
@@ -168,10 +187,51 @@
             // outfitsToolStripMenuItem
             // 
             outfitsToolStripMenuItem.BackColor = SystemColors.MenuHighlight;
+            outfitsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { rockBandToolStripMenuItem, rockBandToolStripMenuItem1, stageToolStripMenuItem, streetToolStripMenuItem, catGirlToolStripMenuItem });
             outfitsToolStripMenuItem.Font = new Font("Juice ITC", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
             outfitsToolStripMenuItem.Name = "outfitsToolStripMenuItem";
             outfitsToolStripMenuItem.Size = new Size(87, 33);
             outfitsToolStripMenuItem.Text = "Outfits";
+            // 
+            // rockBandToolStripMenuItem
+            // 
+            rockBandToolStripMenuItem.BackColor = SystemColors.MenuHighlight;
+            rockBandToolStripMenuItem.Name = "rockBandToolStripMenuItem";
+            rockBandToolStripMenuItem.Size = new Size(180, 34);
+            rockBandToolStripMenuItem.Text = "Default";
+            rockBandToolStripMenuItem.Click += rockBandToolStripMenuItem_Click;
+            // 
+            // rockBandToolStripMenuItem1
+            // 
+            rockBandToolStripMenuItem1.BackColor = SystemColors.MenuHighlight;
+            rockBandToolStripMenuItem1.Name = "rockBandToolStripMenuItem1";
+            rockBandToolStripMenuItem1.Size = new Size(180, 34);
+            rockBandToolStripMenuItem1.Text = "Rock Band";
+            rockBandToolStripMenuItem1.Click += rockBandToolStripMenuItem1_Click;
+            // 
+            // stageToolStripMenuItem
+            // 
+            stageToolStripMenuItem.BackColor = SystemColors.MenuHighlight;
+            stageToolStripMenuItem.Name = "stageToolStripMenuItem";
+            stageToolStripMenuItem.Size = new Size(180, 34);
+            stageToolStripMenuItem.Text = "Stage";
+            stageToolStripMenuItem.Click += stageToolStripMenuItem_Click;
+            // 
+            // streetToolStripMenuItem
+            // 
+            streetToolStripMenuItem.BackColor = SystemColors.MenuHighlight;
+            streetToolStripMenuItem.Name = "streetToolStripMenuItem";
+            streetToolStripMenuItem.Size = new Size(180, 34);
+            streetToolStripMenuItem.Text = "Street";
+            streetToolStripMenuItem.Click += streetToolStripMenuItem_Click;
+            // 
+            // catGirlToolStripMenuItem
+            // 
+            catGirlToolStripMenuItem.BackColor = SystemColors.MenuHighlight;
+            catGirlToolStripMenuItem.Name = "catGirlToolStripMenuItem";
+            catGirlToolStripMenuItem.Size = new Size(180, 34);
+            catGirlToolStripMenuItem.Text = "Cat Girl";
+            catGirlToolStripMenuItem.Click += catGirlToolStripMenuItem_Click;
             // 
             // exitToolStripMenuItem
             // 
@@ -188,12 +248,12 @@
             textBox1.BorderStyle = BorderStyle.None;
             textBox1.Font = new Font("Juice ITC", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
             textBox1.ForeColor = SystemColors.InfoText;
-            textBox1.Location = new Point(520, 290);
+            textBox1.Location = new Point(520, 329);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
             textBox1.ScrollBars = ScrollBars.Vertical;
-            textBox1.Size = new Size(419, 210);
+            textBox1.Size = new Size(419, 171);
             textBox1.TabIndex = 6;
             textBox1.Visible = false;
             textBox1.TextChanged += textBox1_TextChanged;
@@ -229,10 +289,10 @@
             // 
             pictureBox1.BackColor = SystemColors.Control;
             pictureBox1.Image = Properties.Resources.miku1;
-            pictureBox1.Location = new Point(520, 27);
+            pictureBox1.Location = new Point(522, 37);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(488, 336);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Size = new Size(488, 296);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 9;
             pictureBox1.TabStop = false;
             pictureBox1.Visible = false;
@@ -253,8 +313,9 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.BackColor = SystemColors.ButtonHighlight;
+            label5.BackColor = SystemColors.Control;
             label5.Font = new Font("Juice ITC", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.InactiveCaptionText;
             label5.Location = new Point(555, 506);
             label5.Name = "label5";
             label5.Size = new Size(85, 29);
@@ -268,7 +329,7 @@
             label7.AutoSize = true;
             label7.Font = new Font("Juice ITC", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label7.ForeColor = SystemColors.InfoText;
-            label7.Location = new Point(883, 509);
+            label7.Location = new Point(646, 506);
             label7.Name = "label7";
             label7.Size = new Size(23, 29);
             label7.TabIndex = 13;
@@ -279,10 +340,10 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.BackColor = SystemColors.ButtonHighlight;
+            label6.BackColor = SystemColors.Control;
             label6.Font = new Font("Juice ITC", 32F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = SystemColors.InactiveCaptionText;
-            label6.Location = new Point(4, 40);
+            label6.Location = new Point(4, 55);
             label6.Name = "label6";
             label6.Size = new Size(175, 61);
             label6.TabIndex = 14;
@@ -296,7 +357,7 @@
             button3.BackColor = Color.SkyBlue;
             button3.Font = new Font("Kristen ITC", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button3.ForeColor = Color.Black;
-            button3.Location = new Point(2, 103);
+            button3.Location = new Point(0, 135);
             button3.Name = "button3";
             button3.Size = new Size(263, 68);
             button3.TabIndex = 15;
@@ -311,7 +372,7 @@
             button4.BackColor = Color.SkyBlue;
             button4.Font = new Font("Kristen ITC", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button4.ForeColor = Color.Black;
-            button4.Location = new Point(257, 103);
+            button4.Location = new Point(257, 135);
             button4.Name = "button4";
             button4.Size = new Size(263, 68);
             button4.TabIndex = 16;
@@ -324,11 +385,11 @@
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Juice ITC", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label8.Location = new Point(185, 40);
+            label8.Location = new Point(185, 54);
             label8.Name = "label8";
-            label8.Size = new Size(138, 62);
+            label8.Size = new Size(161, 62);
             label8.TabIndex = 17;
-            label8.Text = "Current clicks\r\nper second: ";
+            label8.Text = "Current crystals\r\nper click: ";
             label8.Visible = false;
             label8.Click += label8_Click;
             // 
@@ -337,11 +398,11 @@
             label9.AutoSize = true;
             label9.Font = new Font("Juice ITC", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label9.ForeColor = SystemColors.InactiveCaptionText;
-            label9.Location = new Point(320, 55);
+            label9.Location = new Point(337, 72);
             label9.Name = "label9";
-            label9.Size = new Size(197, 39);
+            label9.Size = new Size(204, 39);
             label9.TabIndex = 18;
-            label9.Text = "clicks per second";
+            label9.Text = "crystals per click";
             label9.Visible = false;
             label9.Click += label9_Click;
             // 
@@ -351,7 +412,7 @@
             button5.BackColor = Color.SkyBlue;
             button5.Font = new Font("Kristen ITC", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button5.ForeColor = Color.Black;
-            button5.Location = new Point(2, 172);
+            button5.Location = new Point(0, 215);
             button5.Name = "button5";
             button5.Size = new Size(263, 68);
             button5.TabIndex = 19;
@@ -366,7 +427,7 @@
             button6.BackColor = Color.SkyBlue;
             button6.Font = new Font("Kristen ITC", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button6.ForeColor = Color.Black;
-            button6.Location = new Point(257, 172);
+            button6.Location = new Point(257, 215);
             button6.Name = "button6";
             button6.Size = new Size(263, 68);
             button6.TabIndex = 20;
@@ -375,12 +436,164 @@
             button6.Visible = false;
             button6.Click += button6_Click;
             // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ButtonHighlight;
+            button7.FlatAppearance.BorderColor = Color.FromArgb(0, 192, 192);
+            button7.FlatAppearance.BorderSize = 2;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Juice ITC", 20F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button7.ForeColor = SystemColors.MenuHighlight;
+            button7.Location = new Point(291, 343);
+            button7.Name = "button7";
+            button7.Size = new Size(372, 49);
+            button7.TabIndex = 21;
+            button7.Text = "Credits";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = SystemColors.Control;
+            pictureBox3.Image = Properties.Resources.miku_leo1;
+            pictureBox3.Location = new Point(519, 37);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(420, 296);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 22;
+            pictureBox3.TabStop = false;
+            pictureBox3.Visible = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = SystemColors.Control;
+            label10.Font = new Font("Juice ITC", 32F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = SystemColors.InactiveCaptionText;
+            label10.Location = new Point(0, 304);
+            label10.Name = "label10";
+            label10.Size = new Size(148, 61);
+            label10.TabIndex = 23;
+            label10.Text = "Outfits";
+            label10.Visible = false;
+            // 
+            // button8
+            // 
+            button8.AllowDrop = true;
+            button8.BackColor = Color.SkyBlue;
+            button8.Font = new Font("Kristen ITC", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.ForeColor = Color.Black;
+            button8.Location = new Point(0, 373);
+            button8.Name = "button8";
+            button8.Size = new Size(263, 68);
+            button8.TabIndex = 24;
+            button8.Text = "Rock Band Outfit\r\nCost: 2000";
+            button8.UseVisualStyleBackColor = false;
+            button8.Visible = false;
+            button8.Click += button8_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = SystemColors.Control;
+            pictureBox4.Image = Properties.Resources.miku_more11;
+            pictureBox4.Location = new Point(519, 37);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(420, 296);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 25;
+            pictureBox4.TabStop = false;
+            pictureBox4.Visible = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // button9
+            // 
+            button9.AllowDrop = true;
+            button9.BackColor = Color.SkyBlue;
+            button9.Font = new Font("Kristen ITC", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.ForeColor = Color.Black;
+            button9.Location = new Point(257, 373);
+            button9.Name = "button9";
+            button9.Size = new Size(263, 68);
+            button9.TabIndex = 26;
+            button9.Text = "Stage Outfit\r\nCost: 5000";
+            button9.UseVisualStyleBackColor = false;
+            button9.Visible = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.AllowDrop = true;
+            button10.BackColor = Color.SkyBlue;
+            button10.Font = new Font("Kristen ITC", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button10.ForeColor = Color.Black;
+            button10.Location = new Point(0, 455);
+            button10.Name = "button10";
+            button10.Size = new Size(263, 68);
+            button10.TabIndex = 27;
+            button10.Text = "Street Outfit\r\nCost: 10,000";
+            button10.UseVisualStyleBackColor = false;
+            button10.Visible = false;
+            button10.Click += button10_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = SystemColors.Control;
+            pictureBox5.Image = Properties.Resources.miku_vbs1;
+            pictureBox5.Location = new Point(519, 37);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(420, 300);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 28;
+            pictureBox5.TabStop = false;
+            pictureBox5.Visible = false;
+            pictureBox5.Click += pictureBox5_Click_1;
+            // 
+            // button11
+            // 
+            button11.AllowDrop = true;
+            button11.BackColor = Color.SkyBlue;
+            button11.Font = new Font("Kristen ITC", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button11.ForeColor = Color.Black;
+            button11.Location = new Point(256, 455);
+            button11.Name = "button11";
+            button11.Size = new Size(263, 68);
+            button11.TabIndex = 29;
+            button11.Text = "Cat Girl Outfit\r\nCost: 50,000";
+            button11.UseVisualStyleBackColor = false;
+            button11.Visible = false;
+            button11.Click += button11_Click;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = SystemColors.Control;
+            pictureBox6.ErrorImage = null;
+            pictureBox6.Image = Properties.Resources.miku_wonderland1;
+            pictureBox6.Location = new Point(519, 37);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(420, 300);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 30;
+            pictureBox6.TabStop = false;
+            pictureBox6.Visible = false;
+            pictureBox6.Click += pictureBox6_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
             ClientSize = new Size(939, 538);
+            Controls.Add(pictureBox6);
+            Controls.Add(button11);
+            Controls.Add(pictureBox5);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(pictureBox4);
+            Controls.Add(button8);
+            Controls.Add(label10);
+            Controls.Add(pictureBox3);
+            Controls.Add(button7);
             Controls.Add(button6);
             Controls.Add(button5);
             Controls.Add(label9);
@@ -409,6 +622,10 @@
             menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -440,5 +657,20 @@
         private Label label9;
         private Button button5;
         private Button button6;
+        private Button button7;
+        private PictureBox pictureBox3;
+        private Label label10;
+        private Button button8;
+        private ToolStripMenuItem rockBandToolStripMenuItem;
+        private ToolStripMenuItem rockBandToolStripMenuItem1;
+        private PictureBox pictureBox4;
+        private ToolStripMenuItem stageToolStripMenuItem;
+        private Button button9;
+        private Button button10;
+        private PictureBox pictureBox5;
+        private ToolStripMenuItem streetToolStripMenuItem;
+        private Button button11;
+        private PictureBox pictureBox6;
+        private ToolStripMenuItem catGirlToolStripMenuItem;
     }
 }
